d73871ce2fff12423f6393377f2fc26c53fec9d0
Geordee Naliyath <geordee@gmail.com>
2023-06-09 09:08:01 +0100

    Well-formed HTML

--- routes/home.go
+++ routes/home.go
@@ -6,6 +6,11 @@ import (
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<html>")
+	fmt.Fprintf(w, "<head><title>Booking App</title></head>")
+	fmt.Fprintf(w, "<body>")
 	fmt.Fprintf(w, "<h1>Booking App</h1>")
 	fmt.Fprintf(w, "<p>Welcome to Booking App!</p>")
+	fmt.Fprintf(w, "</body>")
+	fmt.Fprintf(w, "</html>")
 }
